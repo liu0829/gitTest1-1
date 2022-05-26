c81c92d4a14215795e578ff52e02ea93b11de3c2
lgm0829 <549633991@qq.com>
2022-05-27 01:08:55 +0800

    gitTest1 second commit

--- src/main/java/com/min/gitTest1.java
+++ src/main/java/com/min/gitTest1.java
@@ -3,5 +3,6 @@ package com.min;
 public class gitTest1 {
     public static void main(String[] args) {
         System.out.println("this is the gittest!");
+        System.out.println("this is the gitTest2");
     }
 }
