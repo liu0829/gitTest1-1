66966a3bf8ddb0bc656b2659b9889ad268544332
lgm0829 <549633991@qq.com>
2022-05-27 01:39:02 +0800

    gitTest1 third commit

--- src/main/java/com/min/gitTest1.java
+++ src/main/java/com/min/gitTest1.java
@@ -4,5 +4,6 @@ public class gitTest1 {
     public static void main(String[] args) {
         System.out.println("this is the gittest!");
         System.out.println("this is the gitTest2");
+        System.out.println("third add!");
     }
 }
